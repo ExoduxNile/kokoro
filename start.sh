c1352c91194109a5abb2681d6089e332cbd4f61b
exoduxnile <sandeep.tete2012@gmail.com>
2025-05-10 13:42:44 +0530

    strr

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ if [ ! -f "models/kokoro-v1_0.pth" ]; then
     mkdir -p models
     echo "Downloading model file..."
     curl -L -o models/kokoro-v1_0.pth \
-        "https://objects.githubusercontent.com/github-production-release-asset-2e65be/910061041/182e4328-153b-4ff4-944d-4a6fda3d2d5a?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250509%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250509T220525Z&X-Amz-Expires=300&X-Amz-Signature=11c848a00e4e8de20973310d1711a2276963b860ea1cde9503f7bb9db4e6e9a7&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dkokoro-v1_0.pth&response-content-type=application%2Foctet-stream"
+        "https://github.com/remsky/Kokoro-FastAPI/releases/download/v0.1.4/config.json"
 fi
 
 # Download config file
