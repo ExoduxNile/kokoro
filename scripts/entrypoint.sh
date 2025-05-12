dea9149ceeec3bd2982feb58f40701c55c592d42
exoduxnile <sandeep.tete2012@gmail.com>
2025-05-12 12:42:19 +0530

    entry

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 if [ "$DOWNLOAD_MODEL" = "true" ]; then
     python download_model.py --output api/src/models/v1_0
 fi
-
+pip install uv
 exec uv run --extra $DEVICE --no-sync python -m uvicorn api.src.main:app --host 0.0.0.0 --port 8880 --log-level debug
